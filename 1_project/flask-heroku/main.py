6b05430e3b75f7548af454101818e852f13cebf3
python-leon <hueisam57@gmail.com>
2021-12-12 10:26:55 +0800

    First Deploy

--- 1_project/flask-heroku/main.py
+++ 1_project/flask-heroku/main.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return "Hello Flask 2"
+	return "Hello Flask 3"
 
 @app.route("/test")
 def test():
-	return "This is Test 2"
+	return "This is Test 3"
 
 if __name__=="__main__":
 	app.run()
