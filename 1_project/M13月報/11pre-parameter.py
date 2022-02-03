284d6a004ad098970c39d8c5c5f71d54a17307fa
leon <81945026+leon000694@users.noreply.github.com>
2022-02-03 19:17:03 +0800

    Delete 1_project directory

--- 1_project/M13月報/11pre-parameter.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append('D:/python-training/Tool') # for import dectry
-from T_email_UseGmail import send_gmail
-
-## 每月10日自動發(含gs試算表網址)信 通知各承辦人填報相關數據
-url = 'https://docs.google.com/spreadsheets/d/1VLgx3MrjBtj8KSlO4j4LWN3aignk_ILgKpmnH2vEsD8/edit#gid=0' # 填報表Google Sheet網址
-
-send_gmail(title='請您協助於本月15日前登錄10月份M13相關業務數據。', contents=f'Google雲碟網址: {url}, from M13 Hsu Hui-Sheng.')
--- 1_project/test.py
+++
@@ -1,5 +0,0 @@
-a=3
-b=7
-print(a//3)
-print(b//3)
-print(b%3)
